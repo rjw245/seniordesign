87fecd5d852f3d4ef5147a7f1fbafd0805f88f62
Riley Wood <rjw245@gmail.com>
2016-04-07 20:03:30 -0400

    Added arduino sketch

---
+++ code/pickup_piano/pickup_piano.ino
@@ -0,0 +1,31 @@
+#define A_SENSE         A0
+#define BFLAT_SENSE     A1
+#define B_SENSE         A2
+#define C_SENSE         A3
+#define CSHARP_SENSE    A4
+#define D_SENSE         A5
+#define DSHARP_SENSE    A6
+#define E_SENSE         A7
+#define F_SENSE         A8
+#define FSHARP_SENSE    A9
+#define G_SENSE         A10
+#define GSHARP_SENSE    A11
+
+void setup() {
+    pinMode(A_SENSE, INPUT);
+    pinMode(BFLAT_SENSE, INPUT);
+    pinMode(B_SENSE, INPUT);
+    pinMode(C_SENSE, INPUT);
+    pinMode(CSHARP_SENSE, INPUT);
+    pinMode(D_SENSE, INPUT);
+    pinMode(DSHARP_SENSE, INPUT);
+    pinMode(E_SENSE, INPUT);
+    pinMode(F_SENSE, INPUT);
+    pinMode(FSHARP_SENSE, INPUT);
+    pinMode(G_SENSE, INPUT);
+    pinMode(GSHARP_SENSE, INPUT);
+}
+
+void loop() {
+
+}
