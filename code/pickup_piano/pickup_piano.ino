3aabac445b8e5862f4f36bd3701e389fc12a88d7
Riley Wood <rjw245@gmail.com>
2016-04-21 21:08:49 -0400

    Got arduino code printing in response to one note, note on and note off

--- code/pickup_piano/pickup_piano.ino
+++ code/pickup_piano/pickup_piano.ino
@@ -1,16 +1,21 @@
 #include <DueTimer.h>
 #include "MIDIUSB.h"
 
+#define DEBUG       1
+
 #define ADC_RES     12
 #define ADC_MAX     4095
 
+#define SAMPLE_RATE   10000 //Hz
+#define WINDOW_PERIOD 37000 //usec
+
 //Remember that the midpoint is at ADC_MAX/2, roughly
 #define NOTEON_THRESH  1000
-#define NOTEOFF_THRESH 600 
+#define NOTEOFF_THRESH 850 
 #define CHANNEL        1
 
 //GEN(NAME, PIN, MIDINUM)
-#define FOREACH_NOTE(GEN) \
+#define FOREACH_NOTE_BACKUP(GEN) \
     GEN(A,      A0,     21) \
     GEN(BFLAT,  A1,     22) \
     GEN(B,      A2,     23) \
@@ -22,6 +27,9 @@
     GEN(F,      A8,     29) \
     GEN(FSHARP, A9,     30) \
     GEN(G,      A10,    31) \
+    GEN(GSHARP, A11,    32) \
+
+#define FOREACH_NOTE(GEN) \
     GEN(GSHARP, A11,    32) \
 
 #define GEN_ENUM(NAME, PIN, MIDINUM) NAME,
@@ -52,17 +60,24 @@ const int note_midinum[NUM_NOTES] =
 // the maximum reading seen on each
 // channel for the current sample
 // window
-static int note_max[NUM_NOTES] = { 0 }; //Init all to zero
+static volatile int note_max[NUM_NOTES] = { 0 }; //Init all to zero
 
 //Keeps track of whether a note is currently on
 static bool note_on[NUM_NOTES] = { 0 };
 
-bool window_complete = false;
+volatile bool window_complete = false;
 
 void setup() {
+    #if DEBUG
+      Serial.begin(9600);
+    #endif
     for(int i=0; i<NUM_NOTES; i++) {
         pinMode(note_pins[i],INPUT);
     }
+
+    //Set up timer callbacks
+    Timer3.attachInterrupt(sample_isr).setFrequency(SAMPLE_RATE).start();
+    Timer4.attachInterrupt(sample_window_ended_isr).setPeriod(WINDOW_PERIOD).start();
 }
 
 // ISR to be called at regular interval.
@@ -79,7 +94,7 @@ void sample_isr() {
 
 //Called every time a sample window completes
 //Resets the maximum seen for each note to zero
-void new_sample_window_isr() {
+void new_sample_window() {
     for(int i=0; i<NUM_NOTES; i++) {
         note_max[i] = 0;
     }
@@ -96,14 +111,24 @@ void sample_window_ended_isr()
 void noteOn(byte channel, byte pitch, byte velocity) {
   midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
   MidiUSB.sendMIDI(noteOn);
+  #if DEBUG
+    Serial.println("Note on");
+  #endif
 }
 
 void noteOff(byte channel, byte pitch, byte velocity) {
   midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
   MidiUSB.sendMIDI(noteOff);
+  #if DEBUG
+    Serial.println("Note off");
+  #endif
 }
 
 void loop() {
+  detect_notes();
+}
+
+void detect_notes() {
     if (window_complete) {
         window_complete = false;
 
@@ -122,7 +147,7 @@ void loop() {
                 noteOff(CHANNEL, note_midinum[i], 0);
                 MidiUSB.flush();
             }
-            
+            /*
             //Note on --> on again (hit again before dying off completely)
             //TODO: This has a problem: it will fire repeatedly once a note is on
             else if (note_on[i]  &&  note_max[i] > NOTEON_THRESH) {
@@ -130,12 +155,17 @@ void loop() {
                 noteOn(CHANNEL, note_midinum[i], velocity);
                 MidiUSB.flush();
             }
+            */
+            #if DEBUG
+              Serial.println(note_max[i]);
+            #endif
         }
+        new_sample_window();
     }
 }
 
 //Maps the wave amplitude to a velocity
 int amp_to_vel(int amp) {
-  return amp/2;
+  return sqrt(amp);
 }
 
