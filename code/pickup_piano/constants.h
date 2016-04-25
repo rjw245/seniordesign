2931646484442aad3ac306a937db2429dde15076
Riley Wood <rjw245@gmail.com>
2016-04-24 21:53:36 -0400

    Tweaked constants

--- code/pickup_piano/constants.h
+++ code/pickup_piano/constants.h
@@ -7,10 +7,10 @@
 #define NUM_SAMP  (WINDOW*SAMPLE_RATE)
 
 // Remember that the midpoint is at ADC_MAX/2, roughly
-// TODO: These are probably too high given that we're using RMS now
-#define NOTEON_THRESH   4000
-#define NOTEOFF_THRESH  3400 
-#define CHANNEL         1
+// TODO: Tune these given that we're using RMS now
+#define NOTEON_THRESH   2470
+#define NOTEOFF_THRESH  2260
 
 //MIDI constants
 #define MAXVELOCITY     127
+#define CHANNEL         1
